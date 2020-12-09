d5fd1b8d8490d48015ad862707e3dccbcb7ed75d
dashamag2802 <dashamag@bk.ru>
2020-12-09 17:50:04 -0500

    first commit

---
+++ test/initual.js
@@ -0,0 +1,14 @@
+describe('My Little Hero', function () {
+
+    describe('Getting to the page', function () {
+
+        it('TC-001 Title is correct ', function () {
+            browser.url('');
+            let title = browser.getTitle();
+            browser.pause(2000);
+            expect(title).toEqual('Local Coding');
+        });
+
+    });
+
+});
